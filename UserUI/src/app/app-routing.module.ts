071ef4e84b823007be8945c3c498571e96e66092
elsekily <youssefelsekily@outlook.com>
2023-05-13 06:30:26 +0300

    Adding routing updating module

--- UserUI/src/app/app-routing.module.ts
+++ UserUI/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegesiterUserComponent } from './Components/regesiter-user/regesiter-user.component';
+import { GetUserDataComponent } from './Components/get-user-data/get-user-data.component';
+import { ResetPasswordComponent } from './Components/reset-password/reset-password.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/home/home.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', component: AppComponent, pathMatch:'full'},
+  { path: 'home', component: HomeComponent},
+  { path: 'register', component: RegesiterUserComponent },
+  { path: 'getuser', component: GetUserDataComponent },
+  { path: 'resetpassword', component: ResetPasswordComponent }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule] 
--- UserUI/src/app/app.module.ts
+++ UserUI/src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { RegesiterUserComponent } from './Components/regesiter-user/regesiter-user.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ResetPasswordComponent } from './Components/reset-password/reset-password.component';
+import { HomeComponent } from './Components/home/home.component';
+import { GetUserDataComponent } from './Components/get-user-data/get-user-data.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    RegesiterUserComponent,
+    ResetPasswordComponent,
+    GetUserDataComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    CommonModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
